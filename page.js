5bb86e128bf372c3ad10ff3db1a01b5ed3ba724d
kankaweesaghle <kankaweesaghle@gmail.com>
2026-05-29 13:33:45 +0700

    version 1.0

--- page.js
+++ page.js
@@ -1,1 +1,7 @@
-version 3.0
+echo "# FrontEnd" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kankaweesaghle-cloud/FrontEnd.git
+git push -u origin main
